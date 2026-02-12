3ccecfe1cbf7253566c36456f843f456d559ee58
oeunji <oieunxe@gmail.com>
2026-02-12 21:42:31 +0900

    feat: 애플 펜슬 및 손가락으로 그리기 기능 구현

--- Swift-Student-Challenge.swiftpm/ContentView.swift
+++ Swift-Student-Challenge.swiftpm/ContentView.swift
@@ -1,20 +1,29 @@
 import SwiftUI
+import PencilKit
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+struct DrawingView: UIViewRepresentable {
+    @Binding var canvasView: PKCanvasView
+
+    func makeUIView(context: Context) -> PKCanvasView {
+        canvasView.drawingPolicy = .anyInput // 애플 펜슬 + 손가락
+        canvasView.tool = PKInkingTool(.pen, color: .black, width: 5)
+        return canvasView
     }
+
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {}
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .previewDevice("iPad Pro (12.9-inch) (6th generation)")
-            .previewInterfaceOrientation(.landscapeLeft)
+struct ContentView: View {
+    @State private var canvasView = PKCanvasView()
+    
+    var body: some View {
+        VStack {
+            DrawingView(canvasView: $canvasView)
+            
+            HStack {
+                Button("펜") { canvasView.tool = PKInkingTool(.pen, color: .black, width: 5) }
+                Button("지우개") { canvasView.tool = PKEraserTool(.vector) }
+            }
+        }
     }
 }
